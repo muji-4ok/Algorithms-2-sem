03696a225f2a40b918c7f4a4ff27e2f26671186b
Egor Kutovoy <egorkincbl@gmail.com>
2020-03-20 18:36:31 +0300

    Added tests

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "BigInteger.h"
 #include "Rational.h"
+#include <ctime>
+#include <cstdio>
 
 BigInteger factorial(int n) {
   BigInteger result(1);
@@ -14,32 +16,53 @@ BigInteger factorial(int n) {
 }
 
 int main() {
-  BigInteger b1;
-  BigInteger b2;
-  std::cin >> b1 >> b2;
+//  BigInteger b1;
+//  BigInteger b2;
+//  std::cin >> b1 >> b2;
 //
 //  std::cout << "+: " << (b1 + b2) << '\n';
 //  std::cout << "*: " << (b1 * b2) << '\n';
 //  std::cout << "/: " << (b1 / b2) << '\n';
 //  std::cout << "%: " << (b1 % b2) << '\n';
-  Rational r1(b1, b2);
+//  Rational r1(b1, b2);
+//
+//  std::cout << r1.asDecimal(20) << '\n';
+//  std::cout << r1.toString() << '\n';
+//
+//  BigInteger b3;
+//  BigInteger b4;
+//  std::cin >> b3 >> b4;
+//
+//  Rational r2(b3, b4);
+//
+//  std::cout << r2.asDecimal(20) << '\n';
+//  std::cout << r2.toString() << '\n';
+//
+//  std::cout << "+: " << (r1 + r2) << '\n';
+//  std::cout << "-: " << (r1 - r2) << '\n';
+//  std::cout << "*: " << (r1 * r2) << '\n';
+//  std::cout << "/: " << (r1 / r2) << '\n';
+  std::srand(std::time(nullptr));
 
-  std::cout << r1.asDecimal(20) << '\n';
-  std::cout << r1.toString() << '\n';
+  for (int i = 0; i < 100; ++i) {
+    int size1 = std::rand() % 20 + 1;
+    std::vector<int> buffer1;
 
-  BigInteger b3;
-  BigInteger b4;
-  std::cin >> b3 >> b4;
+    for (int j = 0; j < size1; ++j)
+      buffer1.push_back(std::rand() % BigInteger::RADIX);
 
-  Rational r2(b3, b4);
+    int size2 = std::rand() % 20 + 1;
+    std::vector<int> buffer2;
 
-  std::cout << r2.asDecimal(20) << '\n';
-  std::cout << r2.toString() << '\n';
+    for (int j = 0; j < size2; ++j)
+      buffer2.push_back(std::rand() % BigInteger::RADIX);
 
-  std::cout << "+: " << (r1 + r2) << '\n';
-  std::cout << "-: " << (r1 - r2) << '\n';
-  std::cout << "*: " << (r1 * r2) << '\n';
-  std::cout << "/: " << (r1 / r2) << '\n';
+    BigInteger b1(buffer1, std::rand() % 2);
+    BigInteger b2(buffer2, std::rand() % 2);
+    BigInteger b3 = b1 * b2;
+
+    std::cout << b3 << '\n';
+  }
 
   return 0;
 }
