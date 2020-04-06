2780b16de25b943d2001521b8e53060ad8704308
Egor Kutovoy <egorkincbl@gmail.com>
2020-04-06 12:34:49 +0300

    Added report

--- Task_1_b.cpp
+++ Task_1_b.cpp
@@ -2,6 +2,7 @@
 // Created by egork on 18.02.2020.
 //
 // repository: https://github.com/muji-4ok/Algorithms-2-sem/tree/master
+// report: https://contest.yandex.ru/contest/17228/run-report/31168366/
 #include <iostream>
 #include <vector>
 #include <algorithm>
